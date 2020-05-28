cbf73b403cef9d8d148e1692509d359b0b0c36e5
NoSequel <nosequel@protonmail.com>
2020-05-28 14:02:52 +0200

    fix threading :art:

--- src/main/java/io/github/nosequel/schematic/util/ThreadUtil.java
+++ src/main/java/io/github/nosequel/schematic/util/ThreadUtil.java
@@ -1,42 +1,10 @@
 package io.github.nosequel.schematic.util;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-
 public class ThreadUtil {
 
-    private static Thread currentThread;
-    private static long lastCallTime = System.currentTimeMillis();
-
     private static final int priority = Thread.MIN_PRIORITY;
-    private static final Queue<Callback> queue = new PriorityQueue<>();
-
-    /**
-     * Get the current thread
-     *
-     * @return the current thread
-     */
-    public static Thread getCurrentThread() {
-        final Thread thread = currentThread == null ? new Thread(() -> {
-            if (!queue.isEmpty()) {
-                lastCallTime = System.currentTimeMillis();
-            }
-
-            queue.forEach(Callback::accept);
-
-            try {
-                Thread.sleep(System.currentTimeMillis() - lastCallTime < 10000 ? 1000 : 300);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }, "Schematic-Thread") : currentThread;
-
-        thread.setPriority(priority);
-
-        currentThread = thread;
-
-        return thread;
-    }
+    private static Callback method;
+    private static Thread thread;
 
     /**
      * Execute a method in a runnable
@@ -44,9 +12,17 @@ public class ThreadUtil {
      * @param callback the method
      */
     public static void execute(Callback callback) {
-        final Thread thread = getCurrentThread();
+        if (thread == null) {
+            thread = new Thread(() -> {
 
-        System.out.println("Executing method in \"" + thread.getName() + "\"");
-        queue.add(callback);
+                method.accept();
+                Thread.currentThread().stop();
+
+            }, "Schematic Thread");
+            thread.setPriority(priority);
+        }
+
+        method = callback;
+        thread.resume();
     }
 }
