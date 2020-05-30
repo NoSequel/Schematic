d6632c4def91f4fa85281b0535c915e397e27b93
NoSequel <nosequel@protonmail.com>
2020-05-30 02:57:14 +0200

    fix error caused by spigot... :bug:

--- src/main/java/io/github/nosequel/schematic/impl/ChunkedSchematic.java
+++ src/main/java/io/github/nosequel/schematic/impl/ChunkedSchematic.java
@@ -29,17 +29,13 @@ public class ChunkedSchematic extends Schematic {
         ThreadUtil.execute(() -> {
             final List<SchematicBlock> blocks = this.getBlocks();
 
-            JavaUtils.splitList(blocks, Math.min(350, blocks.size() / 10)).forEach(current -> new Thread(() -> {
-                current.forEach((block -> {
-                    final Location relativeLocation = block.getRelativeLocation(location);
-                    final org.bukkit.block.Block black = relativeLocation.getBlock();
+            JavaUtils.splitList(blocks, Math.min(350, blocks.size() / 10)).forEach(current -> new Thread(() -> current.forEach((block -> {
+                final Location relativeLocation = block.getRelativeLocation(location);
+                final org.bukkit.block.Block black = relativeLocation.getBlock();
 
-                    black.setType(block.getType());
-                    black.setData(block.getData());
-                }));
-
-                Thread.currentThread().interrupt();
-            }).run());
+                black.setType(block.getType());
+                black.setData(block.getData());
+            }))).start());
         });
     }
 }
